50e8a99494e8f8af70957896fe0cc85e9b3648f7
Enzo1141281 <enzosimplicio10@gmail.com>
2024-06-12 09:56:04 -0300

    Update Sketch.js

--- Sketch.js
+++ Sketch.js
@@ -31,7 +31,7 @@ function setup() {
 
 function draw() {
     background(0);
-    mostraBolinha();
+    bolinhadotigas();
     movimentaBolinha();
     verificaColisaoBorda();
     mostraRaquete(xRaquete, yRaquete);
@@ -43,7 +43,7 @@ function draw() {
     incluiPlacar() 
     marcaPonto();
 }
-function mostraBolinha() {
+function bolinhadotigas() {
   circle(xBolinha, yBolinha, diametro);
 }
 
